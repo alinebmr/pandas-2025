62c4e33e06d9cde8e143f8d92e7f53d33ac345af
alinebmr <alinebrito.maracaja@gmail.com>
2026-05-30 00:52:28 -0300

    feat: add day01

--- dia01/01_hello_pandas.py
+++ dia01/01_hello_pandas.py
@@ -1,7 +1,7 @@
-# %%
+#%%
 
 import pandas as pd
 
-# %%
+#%%
 
 pd.Series()
--- dia01/02_series.py
+++ dia01/02_series.py
@@ -3,36 +3,22 @@
 idades = [
     32, 38, 30, 30, 31,
     35, 25, 29, 31, 37,
-    27, 23, 36, 33, 32,
+    27, 23, 36, 33, 32
 ]
 
-media = sum(idades) / len(idades)
-print("Media:",media)
+media = sum(idades) /len(idades)
+print(media)
 
 diffs = 0
 for i in idades:
     diffs += (i - media) ** 2
 
-variancia = diffs / (len(idades)-1)
+variancia = diffs / (len(idades) - 1) 
+print(variancia)
 
-print("Variância:", variancia)
-
-# %%
+#%%
 
 import pandas as pd
 
-idades = [
-    32, 38, 30, 30, 31,
-    35, 25, 29, 31, 37,
-    27, 23, 36, 33, 32,
-]
-
 series_idades = pd.Series(idades)
 series_idades
-
-# %%
-# Estatísticas da séries
-media_idades = series_idades.mean()
-var_idades = series_idades.var()
-summary_idades = series_idades.describe()
-summary_idades
